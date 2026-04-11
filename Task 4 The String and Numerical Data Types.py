057095d84fb90145d141671f22fc6c0c0e2bd36e
RobertvanReenen <robert.vanreenen1992@gmail.com>
2026-04-11 19:07:12 +0200

    Add files via upload

---
+++ Task 4 The String and Numerical Data Types.py
@@ -0,0 +1,82 @@
+word = "attention user"
+number = "6"
+decimal = "1.5"
+truth = True
+variation = False
+
+capital_word = word.capitalize()
+print(capital_word)
+
+print(number)
+
+print(decimal)
+
+print(truth)
+
+print(variation)
+
+word_two = "===hi===user"
+strip_word = word_two.strip("=")
+print(strip_word)
+
+rep_word = word.replace("e", "3")
+print(rep_word)
+
+
+position = word[0:3]
+print(position)
+
+postion = word[0::5]
+print(position)
+
+position = word[-7]
+print(position)
+
+calculations_adding = 25 + 55
+print(calculations_adding)
+
+calculations_subtraction = 53 - 15
+print(calculations_subtraction)
+
+calculations_divisions = 95 / 4
+print(calculations_divisions)
+
+calculations_multiplication = 65 * 7
+print(calculations_multiplication)
+
+calculations_exponents = 85 ** 10
+print(calculations_exponents)
+
+calculations_modulus = 78 % 9
+print(calculations_modulus)
+
+import math
+
+square = math.sqrt(69)
+print(square)
+
+num = 54.341
+
+floor_value = math.floor(num)
+print(floor_value)
+
+ceiling_value = math.ceil(num)
+print(ceiling_value)
+
+print(math.pi)
+
+value = "34"
+print(value)
+print(type(value))
+
+int_value = int(value)
+print(int_value)
+print(type(int_value))
+
+float_value = float(value)
+print(float_value)
+print(type(float_value))
+
+str_value = str(float_value)
+print(str_value)
+print(type(str_value))
