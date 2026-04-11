51b417b25c941f6884e3f8b42c9c3f870cf1ea2f
RobertvanReenen <robert.vanreenen1992@gmail.com>
2026-04-11 17:17:31 +0200

    Add files via upload

---
+++ Task 2 Your first computer program.py
@@ -0,0 +1,36 @@
+greeting_phrase = "Attention user"
+print(greeting_phrase)
+
+#Created greeting phrase Attention user
+
+print("When was the last watering day?")
+
+user_input = input("If the user has not watered for more than 2 days : ")
+print("If not!, " + user_input + " !")
+
+greeting_phrase = "Attention user"
+print(greeting_phrase)
+
+#Created greeting phrase Attention user
+
+print("Is it hot?")
+
+user_input = input("If yes : ")
+print("If it is hot! , " + user_input + " !")
+
+greeting_phrase = "Attention user"
+print(greeting_phrase)
+
+"""
+This is a comment block
+I have changed the wording and corrected spelling
+"""
+
+print("Did the temperature change to lower")
+
+user_input = input("If yes : ")
+print("If the temperature changed to lower ! , " + user_input + " ! ")
+
+
+
+
